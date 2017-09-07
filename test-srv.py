613e3575b354b5b175c1f29313917433fded73e0
Matevz Vucnik <vucnikm@gmail.com>
2017-09-07 10:59:53 +0200

    use hostname

--- test-srv.py
+++ test-srv.py
@@ -1,4 +1,5 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from deploymentsample import DeploymentSample
 
 ADDR = "0.0.0.0"
 PORT = 8000
@@ -7,8 +8,10 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         content = self.rfile.read(content_length)
+        message = "Hello deployed code from "
+        message = DeploymentSample(message).getHelloFromHost()
 
-        message = "Hello from node: " + content + "\n"
+        message = message + ": " + content + "\n"
         self.send_response(200)
         self.send_header('Content-Length', len(str(message)))
         self.end_headers()
